de4ab0585e9a5e3e6af519cbafc27ec5c06dd83c
Anaïs <53227802+Kisakay@users.noreply.github.com>
2024-08-18 18:59:56 +0200

    Change

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,9 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   fetch("https://horizon-gateway.ovh/api/ihorizon/v1/bot")
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("serversCount").innerHTML = `<b>${data.info.servers}</b>`;
-      document.getElementById("membersCount").innerHTML = `<b>${Math.round(data.info.members / 1000)}k</b>`;
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById(
+        "serversCount"
+      ).innerHTML = `<b>${data.info.servers}</b>`;
+      document.getElementById("membersCount").innerHTML = `<b>${Math.round(
+        data.info.members / 1000
+      )}k</b>`;
+      document.getElementById(
+        "categoriesCount"
+      ).innerHTML = `<b>${data.content.category}</b>`;
+      document.getElementById(
+        "commandsCount"
+      ).innerHTML = `<b>${data.content.commands}</b>`;
     })
-    .catch(error => console.error("Error fetching data:", error));
+    .catch((error) => console.error("Error fetching data:", error));
 });
